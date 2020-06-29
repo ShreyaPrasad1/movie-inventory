b602209f80c1e504cda0efedb90984e5fa287701
Shreya Prasad <shreya.prasad@thoughtworks.com>
2020-06-29 13:16:35 +0530

    Shreya/Pujitha created movie snapshot domain and project setup

---
+++ src/main/java/com/movieinventory/domain/MovieSnapshot.java
@@ -0,0 +1,13 @@
+package com.movieinventory.domain;
+
+import java.util.Collections;
+import java.util.Date;
+
+public class MovieSnapshot {
+
+    private String title;
+    private Integer releaseYear;
+    private Date releaseDate;
+    private String director;
+    private Collections ratings;
+}
---
+++ src/test/java/com/movieinventory/domain/MovieSnapshotTest.java
@@ -0,0 +1,7 @@
+package com.movieinventory.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieSnapshotTest {
+
+}
